6d17bd492afff7fa60748dc9391165962f4f396f
Ramon Voges <r.voges@dnb.de>
2024-01-05 16:42:57 +0100

    Erster Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Problem
+
+Um einen Container für den Import der Wasserzeichen-Scans in das Katalogsystem der DNB zu erstellen, müssen alle JPG- und PNG-Dateien aus den verschiedenen, nach Mappen sortierten Verzeichnissen extrahiert und in ein gemeinsames Import-Verzeichnis kopiert werden.
+
+# Lösung
+
+Das Python-Skript erledigt genau das und loggt Fehler in der Datei `copy_log.txt`.
---
+++ extract_files/extract-files.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import logging
+
+def copy_jpg_png_to_destination(source_dir, destination_dir):
+    logging.basicConfig(filename='copy_log.txt', level=logging.ERROR,
+                        format='%(asctime)s - %(levelname)s: %(message)s')
+
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.lower().endswith(('.jpg', '.png')):
+                source_file_path = os.path.join(root, file)
+                destination_file_path = os.path.join(destination_dir, file)
+
+                try:
+                    os.makedirs(os.path.dirname(destination_file_path), exist_ok=True)
+                    shutil.copyfile(source_file_path, destination_file_path)
+                    print(f"Copied '{source_file_path}' to '{destination_file_path}'")
+                except Exception as e:
+                    error_msg = f"Error copying '{source_file_path}': {str(e)}"
+                    logging.error(error_msg)
+                    print(error_msg)
+
+# Replace 'source_directory_path' with the directory containing the images
+# Um B einzubinden: sudo mount -t drvfs B: /mnt/b/
+source_directory_path = '/root/wz/pdf-test/test'
+
+# Replace 'destination_directory_path' with the directory where you want to copy the images
+destination_directory_path = '/root/wz/katalog-import'
+
+copy_jpg_png_to_destination(source_directory_path, destination_directory_path)
